4f7419793a37eaaf856440fdc5806c43b3ea156f
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2018-04-26 17:30:17 +0200

    feat(stores/base.js) store interface created

---
+++ src/stores/base.js.ts
@@ -0,0 +1,19 @@
+interface IStore {
+  /**
+   * @description Authentication.
+   * @returns {Promise<boolean>}
+   */
+  login(configuration: {}): Promise<boolean>;
+
+  /**
+   * @description Retrieve file.
+   * @returns {Promise<{}>}
+   */
+  getFile(): Promise<{}>;
+
+  /**
+   * @description Save file on store.
+   * @returns {Promise<boolean>}
+   */
+  save(): Promise<boolean>;
+}
