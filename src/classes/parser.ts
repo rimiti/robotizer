ff2cc1b1a21a10d44847105b995d64027a254507
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2018-04-26 17:58:56 +0200

    feat(classes/parser.js) parser classe created

---
+++ src/classes/parser.ts
@@ -0,0 +1,50 @@
+export default class Parser {
+  private content: object;
+
+  /**
+   * @description Parse existing and retrieving file.
+   */
+  parse() {
+
+  }
+
+  /**
+   * @description Allow path for bot.
+   * @param {string} bot
+   * @param {string} path
+   */
+  allow(bot: string, path: string) {
+
+  }
+
+  /**
+   * @description Disallow path for bot.
+   * @param {string} bot
+   * @param {string} path
+   */
+  disallow(bot: string, path: string) {
+
+  }
+
+  /**
+   * @description Get formated content.
+   */
+  getFormatedContent() {
+
+  }
+
+  /**
+   * @description Get object content.
+   */
+  getObjectContent() {
+
+  }
+
+  /**
+   * @description Save content into specified store.
+   * @param {string} store
+   */
+  saveContent(store: string) {
+
+  }
+}
