b80362d90fa28d06fe0bb0159832fbd55e7c6905
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2018-04-27 11:18:33 +0200

    feat(stores/local/index.ts) local store class created

---
+++ src/stores/local/index.ts
@@ -0,0 +1,31 @@
+import fs from "fs";
+import IStore from "../istore";
+
+export class Local implements IStore {
+  /**
+   * @description Authentication.
+   * @returns {Promise<void>}
+   */
+  public login(): Promise<void> {
+    return Promise.resolve();
+  }
+
+  /**
+   * @description Save file to local file.
+   * @returns {Promise<void>}
+   */
+  public save(content: string, options: object): Promise<void> {
+    return new Promise((resolve, reject) => {
+      const file = fs.createWriteStream(options.path);
+      file.once("open", () => {
+        file.write(content);
+        file.end();
+        resolve();
+      });
+
+      file.once("error", (e: Error) => {
+        reject(new Error(`Error during saving file: ${e}`));
+      });
+    });
+  }
+}
