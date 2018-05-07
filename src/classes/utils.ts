238ba296c69bac1e27323ae6dd03badf63fb0cb2
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2018-05-07 18:58:17 +0200

    feat(classes/utils.ts) isDisallow method created

--- src/classes/utils.ts
+++ src/classes/utils.ts
@@ -1,6 +1,20 @@
 export default class Utils {
 
+  /**
+   * @description Check if is user agent.
+   * @param {string} data
+   * @returns {boolean}
+   */
   public static isUserAgent(data: string): boolean {
     return /^User-agent$/i.test(data);
+  }
+
+  /**
+   * @description Check if is disallow.
+   * @param {string} data
+   * @returns {boolean}
+   */
+  public static isDisallow(data: string): boolean {
+    return /^Disallow$/i.test(data);
   }
 }
