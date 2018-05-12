0444c4f2d43aaa7bba723d11f07cb27038a1baa4
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2018-05-12 10:11:41 +0800

    feat(classes/parser.ts) parser class in progress

--- src/classes/parser.ts
+++ src/classes/parser.ts
@@ -1,10 +1,66 @@
+import Axios from "axios";
+import * as fs from "fs";
+import Utils from "./utils";
+
 export default class Parser {
-  private content: object;
+  private content: {
+    user_agent: {
+      name?: string,
+      disallow?: [string],
+    },
+  } = {
+    user_agent: {},
+  };
+
+  constructor() {
+
+  }
+
+  async getFile(url: string) {
+    const file = fs.createWriteStream("/tmp/robots.txt");
+    const response = await Axios.get(url)
+      .catch((e) => {
+        throw new Error(`Error during retrieving file process: ${e}`);
+      });
+  }
+
+  getContent(): string {
+    return "User-Agent: *\n" +
+      "Disallow: /map/\n" +
+      "Disallow: /404\n" +
+      "Disallow: /422\n" +
+      "Disallow: /500\n" +
+      "Disallow: /api/\n" +
+      "Disallow: /images/site3/logos-clients/\n" +
+      "User-Agent: google-bot\n" +
+      "Disallow: /map/\n" +
+      "Disallow: /404\n" +
+      "Disallow: /422\n" +
+      "Disallow: /500\n" +
+      "Disallow: /api/\n" +
+      "Disallow: /images/site3/logos-clients/\n" +
+      "Sitemap: https://www.clicrdv.com/fr/sitemap_index.xml";
+  }
 
   /**
    * @description Parse existing and retrieving file.
    */
-  parse() {
+  parse(content = this.getContent()) {
+    const tmp = content.split("\n");
+    tmp.forEach((row, index) => {
+
+      if (Utils.isUserAgent(row)) {
+        this.content.user_agent.name = Utils.first(row);
+      } else if (Utils.isDisallow(row)) {
+
+      } else if (Utils.isSitemap(row)) {
+        this.content.sitemap.name = Utils.first(row);
+
+      } else {
+        // throw new Error(`Unknown row type ${row}`);
+      }
+    });
+    console.log(this.content);
 
   }
 
--- test/manager-spec.ts
+++
@@ -1,7 +0,0 @@
-import test from "ava";
-import { Manager } from "../src";
-
-test("Should test instance name", (t) => {
-  const manager: any = new Manager();
-  t.is(manager.constructor.name, "Manager");
-});
