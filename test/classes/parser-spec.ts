de35d1184d5902dee8640a886ff41167a97ab488
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2018-05-21 00:50:55 +0400

    chore(test/*) unused files removed

--- test/classes/parser-spec.ts
+++
@@ -1,8 +0,0 @@
-import test from "ava";
-import Parser from "../../src";
-
-test("getFormatedContent()", (t) => {
-  const parser = new Parser();
-  console.log(parser.parse().getContent());
-  t.is(true, true);
-});
