ed2b0b088fda00ac99341b74b6e9199b268c6ca8
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2018-05-21 10:24:39 +0400

    chore(test/index-spec.ts) test duplicate case

--- test/index-spec.ts
+++ test/index-spec.ts
@@ -35,3 +35,38 @@ test("Should return robots.txt content (Example 1)", (t) => {
   const parser = new Parser(robotsTxt);
   t.deepEqual(parser.getContent(), result);
 });
+
+test("Should return robots.txt content (Example 2)", (t) => {
+  const result = "User-Agent: google-bot\n" +
+    "Allow: /google-bot-url-to-allow-1\n" +
+    "Allow: /google-bot-url-to-allow-2\n" +
+    "Allow: /google-bot-url-to-allow-3\n" +
+    "Disallow: /google-bot-url-to-disallow-1\n" +
+    "Disallow: /google-bot-url-to-disallow-2\n" +
+    "Disallow: /google-bot-url-to-disallow-3\n" +
+    "Disallow: /google-bot-url-to-disallow-4\n" +
+    "Disallow: /google-bot-url-to-disallow-5\n" +
+    "User-Agent: yahoo-bot\n" +
+    "Allow: /yahoo-bot-url-to-allow-3\n" +
+    "Disallow: /yahoo-bot-url-to-disallow-1\n" +
+    "User-Agent: microsoft-bot\n" +
+    "Disallow: /microsoft-bot-url-to-disallow-1\n" +
+    "Sitemap: https://dimsolution.com/sitemap.xml\n";
+  const parser = new Parser();
+  parser.addDisallow('google-bot', '/google-bot-url-to-disallow-1');
+  parser.addDisallow('google-bot', '/google-bot-url-to-disallow-1');
+  parser.addAllow('google-bot', '/google-bot-url-to-allow-1');
+  parser.addDisallow('yahoo-bot', '/yahoo-bot-url-to-disallow-1');
+  parser.addDisallow('google-bot', '/google-bot-url-to-disallow-2');
+  parser.addSitemap('https://dimsolution.com/sitemap.xml');
+  parser.addDisallow('yahoo-bot', '/yahoo-bot-url-to-disallow-1');
+  parser.addAllow('yahoo-bot', '/yahoo-bot-url-to-allow-3');
+  parser.addDisallow('google-bot', '/google-bot-url-to-disallow-3');
+  parser.addAllow('google-bot', '/google-bot-url-to-allow-2');
+  parser.addDisallow('yahoo-bot', '/yahoo-bot-url-to-disallow-1');
+  parser.addDisallow('google-bot', '/google-bot-url-to-disallow-4');
+  parser.addAllow('google-bot', '/google-bot-url-to-allow-3');
+  parser.addDisallow('microsoft-bot', '/microsoft-bot-url-to-disallow-1');
+  parser.addDisallow('google-bot', '/google-bot-url-to-disallow-5');
+  t.deepEqual(parser.getContent(), result);
+});
